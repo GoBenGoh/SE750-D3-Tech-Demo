4ac0fc801536f5b36c01a504b9cdeff4f849c271
Benjamin Goh <Ben.Goh@chorus.co.nz>
2023-04-01 00:36:17 +1300

    Bugfix first two bars and rotate labels.
    First two bars were not showing. Removed useEffect for it to work.
    Rotated x axis label so they are readable with bars on the page.

--- d3-tech-demo/frontend/src/BarGraph.jsx
+++ d3-tech-demo/frontend/src/BarGraph.jsx
@@ -3,50 +3,52 @@ import * as d3 from 'd3'
 
 function BarGraph() {
   const svgRef = useRef();
-  const [achievements, setData] = useState(["name", 100]);
+  const [achievements, setData] = useState(["placeholder", 100]);
 
   useEffect(() => {
   axios.get("http://localhost:3000/api/csgo")
   .then(response => setData(response.data.achievementpercentages.achievements))
   }, []);
 
-  useEffect(() => {
-    const w = 1920;
-    const h = 500;
-    const svg = d3.select(svgRef.current)
-    .attr('width', w)
-    .attr('height', h)
-    .style('background', '#5A5A5A')
-    .style('margin-top', 50)
-    .style('overflow', 'visible');
+  const halfAchievements = achievements.slice(0,85);
+
+  const w = 1800;
+  const h = 500;
+  const svg = d3.select(svgRef.current)
+  .attr('width', w)
+  .attr('height', h)
+  .style('background', '#5A5A5A')
+  .style('margin-top', 5)
+  .style('overflow', 'visible');
     
-    // scaling
-    const xScale = d3.scaleBand().range([0, w]).padding(0);
-    const yScale = d3.scaleLinear().domain([0, 100]).range([h, 0]); // range is inverted as it goes from top to bottom
+  // scaling
+  const xScale = d3.scaleBand().range([0, w]).padding(0.3);
+  const yScale = d3.scaleLinear().domain([0, 100]).range([h, 0]); // range is inverted as it goes from top to bottom
+  xScale.domain(halfAchievements.map(function(d) { return d.name; }));
 
-    xScale.domain(achievements.map(function(d) { return d.name; }));
-    yScale.domain([0, d3.max(achievements, function(d) { return d.percent; })]);
+  svg.append("g")
+  .attr("transform", "translate(0," + h + ")")
+  .call(d3.axisBottom(xScale))
+  .selectAll("text")  
+  .style("text-anchor", "end")
+  .attr("dx", "-2em")
+  .attr("dy", "-0.7em")
+  .attr("transform", "rotate(-90)");
 
-    svg.append("g")
-         .attr("transform", "translate(0," + h + ")")
-         .call(d3.axisBottom(xScale));
-
-    svg.append("g")
-    .call(d3.axisLeft(yScale).tickFormat(function(d){
-        return d;
-    }).ticks(10));
+  svg.append("g")
+  .call(d3.axisLeft(yScale).tickFormat(function(d){
+    return d;
+  }).ticks(10));
 
     // data for svg
-    svg.selectAll('.bar').data(achievements)
-    .enter()
-    .append("rect")    
-    .attr("class", "bar")
-    .attr("x", function(d) { return xScale(d.name); })
-    .attr("y", function(d) { return yScale(d.percent); })
-    .attr("width", xScale.bandwidth())
-    .attr("height", function(d) { return h - yScale(d.percent); });
-
-  }, [achievements]);
+  svg.selectAll('.bar').data(halfAchievements)
+  .enter()
+  .append("rect")    
+  .attr("class", "bar")
+  .attr("x", function(d) { return xScale(d.name); })
+  .attr("y", function(d) { return yScale(d.percent); })
+  .attr("width", xScale.bandwidth())
+  .attr("height", function(d) { return h - yScale(d.percent); });
 
   return (
     <div className='Graph'>
